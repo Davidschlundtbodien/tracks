d80dfdbbc3f10d0340ef602219ec27771ba050b3
davidschlundtbodien <d.schlundtbodien@gmail.com>
2020-12-21 11:38:12 -0700

    adds authcontext to signup screen

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
 import createDataContext from './createDataContext';
+import trackerApi from '../api/tracker';
 
 const authReducer = (state, action) => {
   switch (action.type) {
@@ -8,8 +9,12 @@ const authReducer = (state, action) => {
 };
 
 const signup = (dispatch) => {
-  return ({email, password}) => {
+  return async ({email, password}) => {
+    try {
+      const response = await trackerApi.post('/signup', {email, password});
+    } catch (err) {
 
+    }
   };
 };
 
