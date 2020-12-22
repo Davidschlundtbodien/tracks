7e686bd9d279eb2d44bea341286765350615e840
davidschlundtbodien <d.schlundtbodien@gmail.com>
2020-12-22 13:53:02 -0700

    adds error handling on signup

--- src/screens/SignupScreen.js
+++ src/screens/SignupScreen.js
@@ -30,6 +30,7 @@ const SignupScreen = ({ navigation }) => {
         autoCapitalize="none"
         autoCorrect={false}
       />
+      {state.errorMessage ? <Text style={styles.errorMessage}>{state.errorMessage}</Text> : null}
       <Spacer>
         <Button title="Sign Up"/>
       </Spacer>
@@ -48,6 +49,12 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     marginBottom: 200
+  },
+  errorMessage: {
+    fontSize:16,
+    color: 'red',
+    marginLeft: 15,
+    marginTop: 15
   }
 });
 
