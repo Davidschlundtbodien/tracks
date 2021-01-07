ce1eb3af5f3ae7af1790471ba31b1387fc1e73eb
davidschlundtbodien <d.schlundtbodien@gmail.com>
2021-01-07 09:08:21 -0700

    adds sign in function to AuthContext

--- src/screens/SigninScreen.js
+++ src/screens/SigninScreen.js
@@ -1,16 +1,19 @@
-import React from 'react';
+import React , { useContext }from 'react';
 import { View, StyleSheet, Text } from 'react-native';
 import AuthForm from '../components/AuthForm';
 import NavLink from '../components/NavLink';
+import { Context } from '../context/AuthContext';
 
 
 const SigninScreen = () => {
+  const { state, signin } = useContext(Context);
+  
   return (
     <View style={styles.container}>
       <AuthForm
         headerText="Sign in to your Account"
-        errorMessage=""
-        onSubmit={() => {}}
+        errorMessage={state.errorMessage}
+        onSubmit={signin}
         submitButtonText="Sign In"
       />
       <NavLink 
