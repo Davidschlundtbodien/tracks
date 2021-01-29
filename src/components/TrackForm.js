1332c9bd1071f416fefa9aba3215b0512dbe10e7
davidschlundtbodien <d.schlundtbodien@gmail.com>
2021-01-29 14:46:08 -0700

    adds trackform component for create track screen

---
+++ src/components/TrackForm.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Input, Button } from 'react-native-elements';
+import Spacer from './Spacer';
+
+const TrackForm = () => {
+  return (
+    <>
+      <Spacer>
+        <Input />
+      </Spacer>
+      <Button title="Start Recordering" />
+    </>
+  );
+};
+
+export default TrackForm;
