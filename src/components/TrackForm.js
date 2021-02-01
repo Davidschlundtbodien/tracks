bece3946d6066811c9cb8849e42ef9802d967a19
davidschlundtbodien <d.schlundtbodien@gmail.com>
2021-02-01 14:00:43 -0700

    sets up trackform buttons and state of recording

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -1,14 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Input, Button } from 'react-native-elements';
 import Spacer from './Spacer';
+import { Context as LocationContext } from '../context/LocationContext';
 
 const TrackForm = () => {
+  const { state: { name, recording }, 
+    startRcording, 
+    stopRecording, 
+    changeName 
+  } = useContext(LocationContext);
+
   return (
     <>
       <Spacer>
-        <Input placeholder="Enter track name" />
+        <Input value={name} onChangeText={changeName} placeholder="Enter track name" />
       </Spacer>
-      <Button title="Start Recordering" />
+      { recording 
+        ? <Button title="Stop" onPress={stopRecording} />
+        : <Button title="Start Recordering" onPress={startRcording} />
+      }
     </>
   );
 };
