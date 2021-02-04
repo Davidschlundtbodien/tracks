74a2690bf71a66133f3e7969ba65cdd62413a7ee
davidschlundtbodien <d.schlundtbodien@gmail.com>
2021-02-04 09:46:39 -0700

    adds save recording button to track create screen

--- src/components/TrackForm.js
+++ src/components/TrackForm.js
@@ -2,6 +2,7 @@ import React, { useContext } from 'react';
 import { Input, Button } from 'react-native-elements';
 import Spacer from './Spacer';
 import { Context as LocationContext } from '../context/LocationContext';
+import { LocationSubscriber } from 'expo-location/build/LocationSubscribers';
 
 const TrackForm = () => {
   const { state: { name, recording }, 
@@ -15,10 +16,18 @@ const TrackForm = () => {
       <Spacer>
         <Input value={name} onChangeText={changeName} placeholder="Enter track name" />
       </Spacer>
-      { recording 
-        ? <Button title="Stop" onPress={stopRecording} />
-        : <Button title="Start Recordering" onPress={startRcording} />
-      }
+      <Spacer>
+        { recording ? (
+          <Button title="Stop" onPress={stopRecording} />
+        ) : ( 
+          <Button title="Start Recordering" onPress={startRcording} />
+        )}
+      </Spacer>
+      <Spacer> 
+        {!recording && LocationSubscriber.length ? (
+          <Button title="Save Recording" />
+        ) : null}
+      </Spacer>
     </>
   );
 };
