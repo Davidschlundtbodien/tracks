94f905b26dbe395a3e3ad475ceaef7bb48593445
davidschlundtbodien <d.schlundtbodien@gmail.com>
2021-02-05 09:17:07 -0700

    adds Automatic Auth in tracker.js

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -5,34 +5,34 @@ import { navigate } from '../navigationRef';
 
 const authReducer = (state, action) => {
   switch (action.type) {
-    case 'add_error':
-      return { ...state, errorMessage: action.payload};
-    case 'signin':
-      return { errorMessage: '', token: action.payload};
-    case 'clear_error_message':
-      return { ...state, errorMessage: ''};
-    case 'signout';
-      return { token: null, errorMessage: ''};
-    default:
-      return state;
+  case 'add_error':
+    return { ...state, errorMessage: action.payload};
+  case 'signin':
+    return { errorMessage: '', token: action.payload};
+  case 'clear_error_message':
+    return { ...state, errorMessage: ''};
+  case 'signout':
+    return { token: null, errorMessage: ''};
+  default:
+    return state;
   }
 };
 
-const tryLocalSignin = dispatch => async () => {
+const tryLocalSignin = dispatch => async() => {
   const token = await AsyncStorage.getItem('token');
   if (token) {
     dispatch({ type: 'signin', payload: token});
-    navigate('TrackList')
+    navigate('TrackList');
   } else {
     navigate('Signup');
   }
 };
 
 const clearErrorMessage = dispatch => () => {
-  dispatch({ type: 'clear_error_message'})
+  dispatch({ type: 'clear_error_message'});
 };
 
-const signup = dispatch => async ({email, password}) => {
+const signup = dispatch => async({email, password}) => {
   try {
     const response = await trackerApi.post('/signup', {email, password});
     await AsyncStorage.setItem('token', response.data.token);
@@ -41,11 +41,11 @@ const signup = dispatch => async ({email, password}) => {
     navigate('TrackList');
 
   } catch (err) {
-    dispatch({type: 'add_error', payload: 'Something went wrong with sign up'})
+    dispatch({type: 'add_error', payload: 'Something went wrong with sign up'});
   }
 };
 
-const signin = dispatch => async ({ email, password }) => {
+const signin = dispatch => async({ email, password }) => {
   try {
     const response = await trackerApi.post('/signin', { email, password});
     await AsyncStorage.setItem('token', response.data.token);
@@ -60,7 +60,7 @@ const signin = dispatch => async ({ email, password }) => {
 };
 
 
-const signout = dispatch => async () => {
+const signout = dispatch => async() => {
   await AsyncStorage.removeItem('token');
   dispatch({ type: 'signout'});
   navigate('loginFlow');
